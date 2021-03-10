8d420be760c5c539c4e5e3e79af88a3889e08e8d
VictorHugoOA <49003557+VictorHugoOA@users.noreply.github.com>
2021-03-09 21:38:35 -0600

    Added Rust cargo package for init lexic analyzer

---
+++ lexic-analyzer/target/debug/deps/lexic_analyzer-7ec3048a1fd8036c.d
@@ -0,0 +1,5 @@
+/mnt/d/data/Documentos/GitHub/compilador-isc8/lexic-analyzer/target/debug/deps/lexic_analyzer-7ec3048a1fd8036c: src/main.rs
+
+/mnt/d/data/Documentos/GitHub/compilador-isc8/lexic-analyzer/target/debug/deps/lexic_analyzer-7ec3048a1fd8036c.d: src/main.rs
+
+src/main.rs:
---
+++ lexic-analyzer/target/debug/lexic-analyzer.d
@@ -0,0 +1,1 @@
+/mnt/d/data/Documentos/GitHub/compilador-isc8/lexic-analyzer/target/debug/lexic-analyzer: /mnt/d/data/Documentos/GitHub/compilador-isc8/lexic-analyzer/src/main.rs
