83871ca0ff5c6d39b8b8ab7259d75a060504e5b5
Victor Hugo Olivetti Alvarez <victoralvarez.vhoa@gmail.com>
2021-05-20 13:39:46 -0500

    Ya casi

---
+++ lexic-analyzer/target/rls/debug/deps/lexic_analyzer-d527790e18846c86.d
@@ -0,0 +1,5 @@
+/home/olivetti/src/Proyectos/compilador-isc8/lexic-analyzer/target/rls/debug/deps/lexic_analyzer-d527790e18846c86.rmeta: src/main.rs
+
+/home/olivetti/src/Proyectos/compilador-isc8/lexic-analyzer/target/rls/debug/deps/lexic_analyzer-d527790e18846c86.d: src/main.rs
+
+src/main.rs:
